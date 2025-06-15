4471e7a48ba9fc1aca05df0976b98e0c24bff384
Brian Maloney <3286425+brian-maloney@users.noreply.github.com>
2025-06-15 17:24:59 -0400

    Update updatepkgs.sh
    Add  intel-lpmd package build

--- updatepkgs.sh
+++ updatepkgs.sh
@@ -43,6 +43,7 @@ aur sync --noconfirm --noview --repo vond --root /tmp/local-repo sedutil
 aur sync --noconfirm --noview --repo vond --root /tmp/local-repo nuttcp
 aur sync --noconfirm --noview --repo vond --root /tmp/local-repo splunkforwarder
 aur sync --noconfirm --noview --repo vond --root /tmp/local-repo fluent-bit
+aur sync --noconfirm --noview --repo vond --root /tmp/local-repo intel-lpmd
 
 gpg --keyserver keyserver.ubuntu.com --receive-keys cfdca245b1043cf2a5f97865ffe87404168bd847 # Pablo Galindo Salgado <pablogsal@gmail.com> for python310
 aur sync --noconfirm --noview --repo vond --root /tmp/local-repo python310
